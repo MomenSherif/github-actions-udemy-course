a0e52fcd68b28b3c172593246de956ff1693fe08
Momen Sherif <momensherif.2019@gmail.com>
2022-01-28 01:23:09 +0200

    test vercel

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. 🚀
+          Edit <code>src/App.js</code> and save to reload. Vercel on Development 🚀
         </p>
         <a
           className="App-link"
