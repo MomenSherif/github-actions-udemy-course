244e9a702fab229a336a911cfc952b61b9c4cf4c
Momen Sherif <momensherif.2019@gmail.com>
2022-01-27 15:01:24 +0200

    fix: add custom release github assets

--- release.config.js
+++ release.config.js
@@ -3,6 +3,15 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          // paths should be files or zipped folders
+          { path: 'build.zip', label: 'Build' },
+          { path: 'coverage.zip', label: 'Coverage' },
+        ],
+      },
+    ],
   ],
 };
